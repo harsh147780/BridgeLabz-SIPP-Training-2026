d0744b60e2ca97d6c14602fed424a46820feca95
Harsh <harshchaudhary1477@gmail.com>
2026-06-11 11:12:55 +0530

    add level2 quetions

---
+++ src/Java/core_java_practice/gcr_codebase/programming_element/Level2/AreaOfTriangle.java
@@ -0,0 +1,18 @@
+package Java.core_java_practice.gcr_codebase.programming_element.Level2;
+
+import java.util.Scanner;
+
+public class AreaOfTriangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double base = sc.nextDouble();
+        double height = sc.nextDouble();
+
+        double areaCm = 0.5 * base * height;
+        double areaInch = areaCm / (2.54 * 2.54);
+
+        System.out.println("The Area of the triangle in sq in is " + areaInch +
+                " and sq cm is " + areaCm);
+    }
+}
