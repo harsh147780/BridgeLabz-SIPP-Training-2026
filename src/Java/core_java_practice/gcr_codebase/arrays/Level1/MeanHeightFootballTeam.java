add13adbfc7fe2c27786bf994339e1eda75aff5b
Harsh <harshchaudhary1477@gmail.com>
2026-06-17 09:25:03 +0530

    add arrays problem

---
+++ src/Java/core_java_practice/gcr_codebase/arrays/Level1/MeanHeightFootballTeam.java
@@ -0,0 +1,15 @@
+package Java.core_java_practice.gcr_codebase.arrays.Level1;
+
+import java.util.*;
+public class MeanHeightFootballTeam {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        double[] heights = new double[11];
+        double sum = 0;
+        for (int i = 0; i < 11; i++) {
+            heights[i] = sc.nextDouble();
+            sum += heights[i];
+        }
+        System.out.println("Mean Height = " + (sum / 11));
+    }
+}
